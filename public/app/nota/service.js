c7f3772881c9e293031a7808743fa9912824350e
marcioc0sta <marciocst2@gmail.com>
2020-11-17 17:28:14 -0300

    feat: add pipe and compose operators

--- public/app/nota/service.js
+++ public/app/nota/service.js
@@ -1,12 +1,13 @@
+import { partialize, pipe } from '../utils/operators.js'
 import { handleStatus } from '../utils/promise-helpers.js'
 
 const API = 'http://localhost:3000/notas'
 
-const sumItems = code => notas =>
-  notas
-    .$flatMap(nota => nota.itens)
-    .filter(item => item.codigo === code)
-    .reduce((total, item) => total + item.valor, 0)
+const getItemFromNotas = notas => notas.$flatMap(nota => nota.itens)
+const filterItemsByCode = (code, items) =>
+  items.filter(item => item.codigo === code)
+const sumItemsValue = items =>
+  items.reduce((total, item) => total + item.valor, 0)
 
 export const notaService = {
   listAll() {
@@ -18,6 +19,11 @@ export const notaService = {
       })
   },
   sumItems(code) {
-    return this.listAll().then(sumItems(code))
+    const sumItems = pipe(
+      getItemFromNotas,
+      partialize(filterItemsByCode, code),
+      sumItemsValue
+    )
+    return this.listAll().then(sumItems)
   },
 }
