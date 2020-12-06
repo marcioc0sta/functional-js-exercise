c3490e1b72dc24a0dcfc930847bd8bdf712a1be9
marcioc0sta <marciocst2@gmail.com>
2020-12-06 16:38:44 -0300

    feat: apply maybe monad

--- public/app/nota/service.js
+++ public/app/nota/service.js
@@ -1,19 +1,21 @@
+import { Maybe } from '../utils/maybe.js'
 import { partialize, pipe } from '../utils/operators.js'
 import { handleStatus } from '../utils/promise-helpers.js'
 
 const API = 'http://localhost:3000/notas'
 
-const getItemFromNotas = notas => notas.$flatMap(nota => nota.itens)
-const filterItemsByCode = (code, items) =>
-  items.filter(item => item.codigo === code)
-const sumItemsValue = items =>
-  items.reduce((total, item) => total + item.valor, 0)
+const getItemFromNotas = notasM =>
+  notasM.map(notas => notas.$flatMap(nota => nota.itens))
+const filterItemsByCode = (code, itemsM) =>
+  itemsM.map(items => items.filter(item => item.codigo === code))
+const sumItemsValue = itemsM =>
+  itemsM.map(items => items.reduce((total, item) => total + item.valor, 0))
 
 export const notaService = {
   listAll() {
     return fetch(API)
       .then(handleStatus)
-      .then(notas => null)
+      .then(notas => Maybe.of(notas))
       .catch(err => {
         console.log(err)
         return Promise.reject('Não foi possível obter notas fiscais')
@@ -25,6 +27,8 @@ export const notaService = {
       partialize(filterItemsByCode, code),
       sumItemsValue
     )
-    return this.listAll().then(sumItems)
+    return this.listAll()
+      .then(sumItems)
+      .then(result => result.getOrElse(0))
   },
 }
